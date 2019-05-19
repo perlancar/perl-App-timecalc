de0e8595ff6f08a88b1504a790d689fd4c7a6336
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-05-19 15:15:14 +0700

    Fix typo in regex and tweak variable declaration

--- lib/App/timecalc.pm
+++ lib/App/timecalc.pm
@@ -13,18 +13,16 @@ our @EXPORT_OK = qw(eval_time_expr);
 sub eval_time_expr {
     my $str = shift;
 
-    my $h = 0;
-    my $m = 0;
+    my ($h, $m, $s) = (0, 0, 0);
 
-    my ($h1, $m1, $h2, $m2);
     $str =~ s{
-                 \s* (?<h1>\d\d?):(?<m1>\d\d?)\s*-s*(?<h2>\d\d?):(?<m2>\d\d?) \s* |
+                 \s* (?<h1>\d\d?):(?<m1>\d\d?)\s*-\s*(?<h2>\d\d?):(?<m2>\d\d?) \s* |
                  \s* \+(?<hplus>\d\d?):(?<mplus>\d\d?) \s* |
                  \s* \-(?<hminus>\d\d?):(?<mminus>\d\d?) \s*
          }{
 
              if (defined $+{h1}) {
-                 ($h1, $m1, $h2, $m2) = ($+{h1}, $+{m1}, $+{h2}, $+{m2});
+                 my ($h1, $m1, $h2, $m2) = ($+{h1}, $+{m1}, $+{h2}, $+{m2});
                  if ($h2 < $h1 || $h2 <= $h1 && $m2 <= $m1) {
                      $h2 += 24;
                  }
